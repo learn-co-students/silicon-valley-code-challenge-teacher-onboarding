b5abc3ae449a268b01c177d93294bba6a309f23b
campbelllssoup <matjocampbell@gmail.com>
2019-09-20 11:48:49 -0700

    Finished

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,17 @@
 class FundingRound
+  attr_reader :startup, :vc, :type, :investment
+  @@all = []
+  
+  def initialize(startup, vc, type, investment)
+    @startup = startup
+    @vc = vc
+    @type = type
+    @investment = investment.to_f
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,47 @@
 class VentureCapitalist
+  attr_reader :name
+  attr_accessor :total_worth
+  @@venture_capitalists = []
+  
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@venture_capitalists << self
+  end
+
+  def self.all
+    @@venture_capitalists
+  end
+
+  def self.tres_commas_club
+    tres = @@venture_capitalists.select{|vc| vc.total_worth >= 1000000000}
+    tres_values = tres.map{|vc| vc.total_worth }
+    return tres_values.reduce(:+)
+  end
+
+  def offer_contract(startup, investment_type, amount)
+    offer = FundingRound.new(startup, self, investment_type, amount.to_f)
+    return offer
+  end
+
+  def funding_rounds
+    FundingRound.all.select{|fr| fr.vc == self}
+  end
+
+  def portfolio
+    companies = self.funding_rounds.map{|fr| fr.startup}.uniq
+    return companies
+  end
+
+  def biggest_investment
+    self.funding_rounds.max{|a,b| a.investment <=> b.investment}
+  end
+
+  def invested(domain)
+    total_invested_in_domain = self.funding_rounds.select{|fr| fr.startup.domain == domain}
+    total_invested_in_domain.map!{|fr| fr.investment}
+    answer = total_invested_in_domain.reduce(:+)
+    return answer ? answer : 0
+  end
 
 end
